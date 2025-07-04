7ebbe8e8504d97b9145e2add79df2b07203821e4
Brayan4378 <ocampobrayan74@gmail.com>
2025-07-03 19:22:20 -0500

    Add files via upload

---
+++ TallerPractDos/src/main/java/consDiag/Diagnostico.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package consDiag;
+
+/**
+ *
+ * @author BrayanOcampo
+ */
+public class Diagnostico {
+    
+    //atributos protegidos de la clase diagnostico
+    protected String descripcion;
+    protected String tratamiento;
+
+
+    //constructor que inicializa los atributos con los valores proporcionados
+    public Diagnostico(String descripcion, String tratamiento) {
+        this.descripcion = descripcion;
+        this.tratamiento = tratamiento;
+    }
+    
+    //metodo que verifica si una descripción es considerada larga
+    public boolean esTratamientoLargo(){
+    return tratamiento.length() > 30;
+    }
+
+    //metodo getter para obtener la descripción del diagnóstico
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+     //metodo getter para obtener la descripción del tratamiento
+    public String getTratamiento() {
+        return tratamiento;
+    }
+    
+}
